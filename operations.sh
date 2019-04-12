0595a9cebce90c1f254a067c2e6abf6153a6c6f4
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-04-12 22:21:45 +0200

    Create operations.sh

---
+++ operations.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+export NOM_DU_RESEAU_DEMO_CAMUNDA=reseau_camunda
+
+# Camunda Engine
+export NUM_PORT_ECOUTE_CAMUNDA_ENGINE_IN_CONTAINER=8080
+export NUM_PORT_ECOUTE_CAMUNDA_ENGINE=8091
+
+# Camunda Web client
+export NUM_PORT_ECOUTE_CAMUNDA_WEB_IN_CONTAINER=NUM_PORT_ECOUTE_CAMUNDA_WEB_IN_CONTAINER_JINJA2_VAR
+export NUM_PORT_ECOUTE_CAMUNDA_WEB=NUM_PORT_ECOUTE_CAMUNDA_WEB_JINJA2_VAR
+
+
+
+# 
+# >>>>>  Interpolation de l'environnement Docker.
+# 
+
+# Camunda Engine
+sed -i "s#NUM_PORT_ECOUTE_CAMUNDA_ENGINE_IN_CONTAINER_JINJA2_VAR#$NUM_PORT_ECOUTE_CAMUNDA_ENGINE_IN_CONTAINER#g" ./.env
+sed -i "s#NUM_PORT_ECOUTE_CAMUNDA_ENGINE_JINJA2_VAR#$NUM_PORT_ECOUTE_CAMUNDA_ENGINE#g" ./.env
+# Camunda Web client
+sed -i "s#NUM_PORT_ECOUTE_CAMUNDA_WEB_IN_CONTAINER_JINJA2_VAR#$NUM_PORT_ECOUTE_CAMUNDA_WEB_IN_CONTAINER#g" ./.env
+sed -i "s#NUM_PORT_ECOUTE_CAMUNDA_WEB_JINJA2_VAR#$NUM_PORT_ECOUTE_CAMUNDA_WEB#g" ./.env
+
+# 
+# Fichiers interpolés secondaires : 
+# => Il s'agit des fichiers qui utilisent des variables qui ne peuvent être interpolées par l'environnement docker.
+# 
+sed -i "s#NOM_DU_RESEAU_DEMO_CAMUNDA_JINJA2_VAR#$NOM_DU_RESEAU_DEMO_CAMUNDA#g" ./docker-compose.yml
+sed -i "s#NOM_DU_RESEAU_DEMO_CAMUNDA_JINJA2_VAR#$NOM_DU_RESEAU_DEMO_CAMUNDA#g" ./.env
+
+

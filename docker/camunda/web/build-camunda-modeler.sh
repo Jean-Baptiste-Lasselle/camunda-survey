5a8ce7de7e6662a97a2490d7ab88483405c0e379
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-04-13 03:01:44 +0200

    Update build-camunda-modeler.sh

--- docker/camunda/web/build-camunda-modeler.sh
+++ docker/camunda/web/build-camunda-modeler.sh
@@ -7,27 +7,18 @@ echo "VERIF [NUM_PORT_ECOUTE_CAMUNDA_WEB_IN_CONTAINER=$NUM_PORT_ECOUTE_CAMUNDA_W
 npm install --save-dev cross-env
 npm install --save-dev webpack
 npm install --save-dev webpack-cli
-npm audit fix
 npm install --save-dev case-sensitive-paths-webpack-plugin
-npm audit fix
 npm install --save-dev copy-webpack-plugin
-npm audit fix
 npm install --save-dev license-webpack-plugin
-
 npm install --save-dev babel-loader
-npm audit fix
 npm install --save-dev @babel/core
-npm audit fix
 npm install --save-dev @babel/plugin-syntax-dynamic-import
-npm audit fix
 npm install --save-dev @babel/plugin-proposal-class-properties
-
-npm audit fix
 
 
 # install dependencies
 # plus a few new dev dependencies, due to new transitive resolutions to be automatically resolved, instead of
 # finding them one after the other
 npm install 
-
+npm audit fix
 npm run build
